1ce29539620f2e6aacbbe0bac3e489f6d9bf2df5
corentinclement <corentinclement@pm.me>
2021-09-06 15:09:06 +0200

    Integration of http basic

--- src/main/java/com/greta/cda/library/security/WebSecurityConfig.java
+++ src/main/java/com/greta/cda/library/security/WebSecurityConfig.java
@@ -12,6 +12,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/sign-up").permitAll()
                 .anyRequest().authenticated()
